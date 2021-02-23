ebc3311eb7c461f26d2b169a424f465f1659ca37
ederson <66290546+ederson-cinnamon@users.noreply.github.com>
2021-02-24 01:45:22 +0800

    add server on nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
+  server: {
+    port: 3000, // default: 3000
+    host: "0.0.0.0", // default: localhost
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
